905d4b279e8a9a9a803aedc7a76e9cc59249e386
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-04-22 09:13:13 -0700

    Update backup script.

--- backup_github.sh
+++ backup_github.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
- 
+#!/usr/bin/env bash
+
 # Declare an array of string with type
 declare -a StringArray=("v-sekai-fire" "v-sekai" "godot-extended-libraries" "fire" "lyuma" )
- 
+
 export BACKUP_GITHUB_DATE=`date --iso=min --utc`
 
 # Iterate the string array using for loop
 for val in ${StringArray[@]}; do
    mkdir -p ../$BACKUP_GITHUB_DATE/$val
    cd ../$BACKUP_GITHUB_DATE/$val
-   /mingw64/bin/gh repo list $val --limit 9999999 | awk '{print $1; }' | xargs -L1 /mingw64/bin/gh repo clone
+   gh repo list $val --limit 9999999 | awk '{print $1; }' | xargs -L1 gh repo clone
    cd -
 done
