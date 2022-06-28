e9f3cfe2ccb6e0f2e3f56e9cf062f38645f8eb7a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-06-28 12:45:32 -0700

    Update scripts for new workflow.

--- release_godot_v_sekai.sh
+++ release_godot_v_sekai.sh
@@ -4,8 +4,7 @@ set -e
 
 ORIGINAL_BRANCH=main
 MERGE_REMOTE=v-sekai-godot
-MERGE_BRANCH=groups-staging-4.x
-MERGE_BRANCH_SHARED=groups-shared-4.x
+MERGE_BRANCH=groups-4.x
 DRY_RUN=0
 
 while [[ -n "$1" ]]; do
@@ -39,43 +38,14 @@ add_remote ()
 }
 
 #
-add_remote SaracenOne https://github.com/SaracenOne/godot
-add_remote lyuma https://github.com/lyuma/godot
-add_remote fire https://github.com/fire/godot
 add_remote v-sekai-godot git@github.com:V-Sekai/godot.git
-add_remote BastiaanOlij https://github.com/BastiaanOlij/godot.git
-add_remote tokage https://github.com/TokageItLab/godot.git
-add_remote reduz https://github.com/reduz/godot
-add_remote briansemrau https://github.com/briansemrau/godot.git
-add_remote Faless https://github.com/Faless/godot.git
-add_remote groud https://github.com/groud/godot.git
-add_remote jonbonazza https://github.com/jonbonazza/godot.git
-add_remote Chaosus https://github.com/Chaosus/godot.git
-add_remote clayjohn https://github.com/clayjohn/godot.git
-add_remote nikitalita https://github.com/nikitalita/godot.git
-add_remote NNesh https://github.com/NNesh/godot.git
-add_remote Calinou https://github.com/Calinou/godot.git
-add_remote AnilBK https://github.com/AnilBK/godot.git
-add_remote bruvzg https://github.com/bruvzg/godot.git
-add_remote timothyqiu https://github.com/timothyqiu/godot.git
-add_remote Calinou https://github.com/Calinou/godot.git
-add_remote Zylann https://github.com/Zylann/godot.git
-add_remote techiepriyansh https://github.com/techiepriyansh/godot.git
-add_remote adamscott https://github.com/adamscott/godot.git
-add_remote Geometror https://github.com/Geometror/godot.git
-add_remote YakoYakoYokuYoku https://github.com/YakoYakoYokuYoku/godot
-add_remote WindyDarian https://github.com/WindyDarian/godot/
-add_remote vnen https://github.com/vnen/godot/
-add_remote snailrhymer https://github.com/snailrhymer/godot
-add_remote KoBeWi https://github.com/KoBeWi/godot.git
-add_remote Shnazzy https://github.com/Shnazzy/godot.git
 #
 
 
 merge_branch () {
     git checkout $ORIGINAL_BRANCH --force
     git branch -D $MERGE_BRANCH || true
-    python3 ./thirdparty/git-assembler -av --recreate --config gitassembly-staging
+    python3 ./thirdparty/git-assembler -av --recreate --config gitassembly
     git checkout $MERGE_BRANCH -f
     export MERGE_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
     export MERGE_TAG=$(echo $MERGE_BRANCH.$MERGE_DATE | tr ':' ' ' | tr -d ' \t\n\r')
@@ -102,8 +72,6 @@ has_changes=0
 git diff --quiet HEAD || has_changes=1
 git stash
 merge_branch
-#export MERGE_BRANCH=$MERGE_BRANCH_SHARED
-#merge_branch
 echo -e "ALL DONE. ----------------------------"
 if [[ $has_changes -ne 0 ]]; then
 	echo "Note that any uncommitted changes to the merge script may have been stashed. Run"
