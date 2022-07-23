b1dd2ec571201c35e93e2ceadef09bb2175c6e70
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-07-23 07:28:58 -0700

    Remove mutually exclusive options.

--- backup_github.sh
+++ backup_github.sh
@@ -11,6 +11,6 @@ export BACKUP_GITHUB_DATE=`date --iso=min --utc`
 for val in ${StringArray[@]}; do
    mkdir -p ../$BACKUP_GITHUB_DATE/$val
    cd ../$BACKUP_GITHUB_DATE/$val
-   gh repo list $val --limit 9999999 | awk '{print $1; }' | xargs -L1 -I {} gh repo clone {} -- --recurse-submodules
+   gh repo list $val --limit 9999999 | awk '{print $1; }' | xargs -I {} gh repo clone {} -- --recurse-submodules
    cd -
 done
