c414a2c89e6c7c70b423671e395058218a8fc0c7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-07-31 20:08:08 -0700

    Add code formatting.

--- update_godot_v_sekai.sh
+++ update_godot_v_sekai.sh
@@ -71,6 +71,7 @@ add_remote KoBeWi https://github.com/KoBeWi/godot.git
 add_remote Shnazzy https://github.com/Shnazzy/godot.git
 add_remote Rindbee https://github.com/Rindbee/godot.git
 add_remote aaronfranke https://github.com/aaronfranke/godot.git
+add_remote Razoric480 https://github.com/Razoric480/godot.git
 #
 
 
