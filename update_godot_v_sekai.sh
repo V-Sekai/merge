a2195c271519defec77b47238fec528eaa578216
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-12-02 19:53:32 -0800

    Merge gltf physics.

--- update_godot_v_sekai.sh
+++ update_godot_v_sekai.sh
@@ -46,6 +46,7 @@ add_remote WindyDarian https://github.com/WindyDarian/godot/
 add_remote bruvzg https://github.com/bruvzg/godot.git
 add_remote haasanen https://github.com/haasanen/godot.git
 add_remote TokageItLab https://github.com/TokageItLab/godot.git
+add_remote aaronfranke https://github.com/aaronfranke/godot.git
 #
 
 merge_branch () {
