14e413b3b79e6a2861ac454ab798b0fc5a184f1b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-13 08:46:21 -0700

    Update update_godot_v_sekai.sh

--- update_godot_v_sekai.sh
+++ update_godot_v_sekai.sh
@@ -52,7 +52,7 @@ add_remote clayjohn https://github.com/clayjohn/godot.git
 add_remote Zylann https://github.com/Zylann/godot.git
 add_remote reduz https://github.com/reduz/godot.git
 add_remote adamscott https://github.com/adamscott/godot.git
-add_remote RhubarbVR-godot-lib https://github.com/RhubarbVR/godot-lib.
+add_remote RhubarbVR-godot-lib https://github.com/RhubarbVR/godot-lib.git
 add_remote KoBeWi https://github.com/KoBeWi/godot.git
 #
 #
