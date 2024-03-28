d3e6c2dc2fd40866c677fac7a59c0f0982d1c9d6
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-03-28 04:36:38 -0700

    Merge new remote repository 'bruvzg' for Godot updates. Add to update script.

--- update_godot_v_sekai.sh
+++ update_godot_v_sekai.sh
@@ -47,6 +47,7 @@ add_remote BastiaanOlij https://github.com/BastiaanOlij/godot.git
 add_remote tdaven https://github.com/tdaven/godot.git
 add_remote lyuma https://github.com/lyuma/godot.git
 add_remote Riteo https://github.com/Riteo/godot.git
+add_remote bruvzg https://github.com/bruvzg/godot.git
 #
 
 merge_branch () {
