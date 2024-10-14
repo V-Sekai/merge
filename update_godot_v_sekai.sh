49419a35e757a9622507824cc2f016135ca9664c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-10-14 10:22:57 -0700

    Add missing resources fix.

--- update_godot_v_sekai.sh
+++ update_godot_v_sekai.sh
@@ -42,6 +42,7 @@ add_remote v-sekai-godot https://github.com/V-Sekai/godot.git
 add_remote beicause https://github.com/beicause/godot.git
 add_remote godotengine https://github.com/godotengine/godot.git
 add_remote TokageItLab https://github.com/TokageItLab/godot.git
+add_remote nikitalita https://github.com/nikitalita/godot.git
 #
 
 merge_branch () {
