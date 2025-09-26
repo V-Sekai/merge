e824383c06f69985f265a89495bb7162bbebb82a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-09-25 18:43:08 -0700

    Add remote for zeux repository in update_godot_v_sekai script

--- update_godot_v_sekai.sh
+++ update_godot_v_sekai.sh
@@ -42,6 +42,7 @@ add_remote ()
 add_remote v-sekai-godot https://github.com/V-Sekai/godot.git
 add_remote godotengine https://github.com/godotengine/godot.git
 add_remote PizzaLovers007 https://github.com/PizzaLovers007/godot.git
+add_remote zeux https://github.com/zeux/godot.git
 #
 
 merge_branch () {
